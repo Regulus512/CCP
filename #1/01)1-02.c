5d38c58885dbce515e6b28cadd210946d45a317c
Regulus512 <minji010402@gmail.com>
2025-02-13 15:01:53 +0900

    #1 - 01)1-02 예제

---
+++ #1/01)1-02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("(1) %d x %03d = %5d[end]\n", 3, 24, 3 * 24);
+	printf("(2) %f, %.10f[end]\n", 0.123456789, 0.123456789);
+	printf("(3) %c, %3c[end]\n", 97, 65);
+	printf("(4) %s, %10s, %-10s[end]\n", "string", "right", "left");
+	printf("(5) %d x %d + %d = %d[end]\n", 10, 0b1010, 012, 0x6e);
+	
+
+	// Literal Test
+	int a = 0b1010;
+	printf("%d", ++a);
+}
